6ef654f7195f13a5b42ef3cf043e8dd0017afc51
Giovana Moreira dos Santos <giovana.santosgms@gmail.com>
2026-04-20 16:08:35 -0300

    adicionando testes

---
+++ simple-java-app/src/test/MainTest.java
@@ -0,0 +1,27 @@
+public class MainTest {
+
+    @Test
+    void testSoma() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    void testSubtracao() {
+        assertEquals(2, 5 - 3);
+    }
+
+    @Test
+    void testMultiplicacao() {
+        assertEquals(6, 2 * 3);
+    }
+
+    @Test
+    void testDivisao() {
+        assertEquals(2, 4 / 2);
+    }
+
+    @Test
+    void testBooleano() {
+        assertTrue(5 > 1);
+    }
+}
